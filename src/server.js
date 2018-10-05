94796436d02e028e527ec8b5b0736088fbdc0e61
Henning Håkonsen <henninghaakonsen@me.com>
2018-10-05 14:30:09 +0200

    La til flere ci-tester for enkel, komplett og ekstrem søknad

--- src/server.js
+++ src/server.js
@@ -2,40 +2,25 @@ import bodyParser from 'body-parser';
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import App from './app/App';
-import fs from 'fs';
 
 const api = '/api';
 const express = require('express');
 const server = express();
 const port = process.env.PORT || 9000;
-const mock_path = './src/mock/'
-const SKRIV_OPPSUMMERING_TIL_MOCK_JSON = false;
 
 server.use(bodyParser.json())
 
 server.post(api + '/generateHtml', (req, res) => {
     const oppsummering = req.body;
     const appString = renderToStaticMarkup(<App oppsummering={oppsummering}/>);
-
-    if (process.env.NODE_ENV === 'development') {
-        if (SKRIV_OPPSUMMERING_TIL_MOCK_JSON) {
-            fs.writeFile(mock_path + "oppsummering.json", JSON.stringify(oppsummering, null, 4), 'utf8', function (err) {
-                if(err) {
-                    return console.log(err);
-                }
-            
-                console.log(`Søknad json ble lagret til '${mock_path}'`);
-            });
-        }
-    }
     
     res.set('Content-Type', 'text/html');
     res.send(new Buffer(appString));
 });
 
-server.get(api + '/generateHtml', (req, res) => {
+server.get('/test/getHtml/:type', (req, res) => {
     try {
-        const oppsummering = require('./mock/oppsummering.json')
+        const oppsummering = require(`./mock/oppsummering_${req.params.type}.json`)
         oppsummering.innsendingsTidspunkt = 'innsendingsTidspunkt'
         const appString = renderToStaticMarkup(<App oppsummering={oppsummering}/>, "utf-8");
 
