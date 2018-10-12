7ff971277f39c5a5eda8ef7fca3bcdfc72cfa63b
Henning Håkonsen <henninghaakonsen@me.com>
2018-10-12 10:25:02 +0200

    Fjernet kravTilSoker og utenlandskKontantstotte komponenter.

--- src/app/SoknadPdf.jsx
+++ src/app/SoknadPdf.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import NavIkon from './ikoner/NavIkon';
 
-import SokerKrav from './SokerKrav';
 import GeneriskBolk from './GeneriskBolk';
 import TilknytningTilUtland from './TilknytningTilUtland';
 import ArbeidIUtlandet from './ArbeidIUtlandet';
-import UtenlandskKontantstotte from './UtenlandskKontantstotte';
 import Oppsummering from './Oppsummering';
 import SoknadsInfo from "./SoknadsInfo";
 
@@ -48,14 +46,10 @@ const SoknadPdf = (props) => {
                 bolker.map(bolk => {
                     if (bolk.elementer === null || bolk.elementer === 0) {
                         switch (bolk.bolknavn) {
-                            case 'kravTilSoker':
-                                return <SokerKrav kravTilSoker={soknad.kravTilSoker} tekster={tekster} />
                             case 'tilknytningTilUtland':
                                 return <TilknytningTilUtland tilknytningTilUtland={soknad.tilknytningTilUtland} familieforhold={soknad.familieforhold} tekster={tekster} />
                             case 'arbeidIUtlandet':
                                 return <ArbeidIUtlandet arbeidIUtlandet={soknad.arbeidIUtlandet} familieforhold={soknad.familieforhold} tekster={tekster} />
-                            case 'utenlandskKontantstotte':
-                                return <UtenlandskKontantstotte utenlandskKontantstotte={soknad.utenlandskKontantstotte} tekster={tekster} />
                             case 'oppsummering':
                                 return <Oppsummering tekster={tekster} />
                             default:
