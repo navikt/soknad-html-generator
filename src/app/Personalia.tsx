56a54a019a046877fcce57e408a7894873297276
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-03-04 09:42:29 +0100

    SD-766: Legg til statsborgerskap

--- src/app/Personalia.tsx
+++ src/app/Personalia.tsx
@@ -3,13 +3,14 @@ import * as React from 'react';
 interface IPeronaliaProps {
     navn: string;
     fnr: string;
+    statsborgerskap: string;
 }
 
-const Personalia: React.FunctionComponent<IPeronaliaProps> = ({ navn, fnr }) => {
+const Personalia: React.FunctionComponent<IPeronaliaProps> = ({ navn, fnr, statsborgerskap }) => {
     return (
         <div className={'personalia'}>
             <h2>{navn}</h2>
-            {fnr}
+            {fnr + ' // ' + statsborgerskap}
         </div>
     );
 };
